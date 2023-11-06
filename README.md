11f7b3617ebf3a742aa48448d484fd00ee89c945
erikmr <erik.martinez@grupokc.com.mx>
2023-11-06 11:13:54 -0600

    🚑 cors

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 Api en Pyton
 
+Para ejecutar localmente hay que ejecutar el ambiente virtual y despues correr flask
+
+```
+. .venv/bin/activate
+flask --app app run
+```
+
+Para detener 
+
 Activar . .venv/bin/activate
 
-Ejecutar Para activar virtual enviroment . .venv/bin/activate Para modo debug `export FLASK_DEBUG=1` Para desactivar virtual enviroment ejecutar comando `deactivate` flask --app app run
+Ejecutar Para activar virtual enviroment . .venv/bin/activate 
+
+Para modo debug `export FLASK_DEBUG=1` 
+
+Para desactivar virtual enviroment ejecutar comando `deactivate` flask --app app run
 
 Actualizar dependencias pip3 freeze &gt; requirements.txt
--- app.py
+++ app.py
@@ -3,23 +3,59 @@ from datetime import datetime
 import requests
 import json 
 import resend
+from flask_cors import CORS,cross_origin
 
-resend.api_key = "re_gTA3ZJ2C_Gh7PpuMSGxqDHFNhCBFkAAwd"
 
-app = Flask(__name__)
+resend.api_key = "re_j7FZ9Rme_4ov44DUobE17FZQEeHR1P46v"
 
+app = Flask(__name__,static_url_path='/static')
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 @app.route('/')
 def index():  
     hora_actual = datetime.now()
     return render_template("index.html",hora_actual=hora_actual)
 
+@app.route("/confirma_vacuna", methods=['POST'])
+@app.route('/confirma_vacuna')
+def confirma_vacuna():  
+    
+    url  = "https://app.vacunainfluenza.com.mx/api/Nf_Data/ExecuteForCRUD"
+  
+    headers = {
+        "content-type": "application/x-www-form-urlencoded;charset=utf-8"
+    }
+    params = request.get_json()
+
+    data = 'ClassName=Vi_Registro_Respuestas_Confirmacion'
+    data += '&Action=Get'
+    data += '&Registro=' +params["registro"]
+    data += '&db=Prometeo_Redes'
+    data += '&GetDataModel="false"'
+    data += '&GetDataTable="false"'
+    print(data)
+    res = requests.post(url,data=data, headers=headers)
+    response = json.loads(res.text)
+    html =  render_template("confirma_vacuna.html",data=response["data"])
+    r = resend.Emails.send({
+        "from": "soporte@vacunainfluenza.com.mx",
+        "to": response["data"][0]["Email"],
+        "subject": "Confirmación de registro aplicación vacuna Influenza 2023",
+        "html": html
+    })
+    return r
+
+
+@app.route("/send_mail", methods=['POST'])
 @app.route('/send_mail')
 def send_mail():
+    hora_actual = datetime.now()
+    html =  render_template("confirma_vacuna.html",hora_actual=hora_actual)
     r = resend.Emails.send({
-        "from": "onboarding@resend.dev",
-        "to": "sistemas@grupokc.com.mx",
+        "from": "soporte@vacunainfluenza.com.mx",
+        "to": "emartinez@ensamble.dev",
         "subject": "Prueba",
-        "html": "<p><a href='https://www.grupokc.com.mx'>vinculo</a>Congrats on sending your <strong>first email</strong>!</p>"
+        "html": html
     })
     print(r)
     return r
